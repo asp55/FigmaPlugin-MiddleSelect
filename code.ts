f43971f608272312390bbf5e659f80aa9a388166
Andrew Parnell <aparnell@gmail.com>
2021-04-30 14:04:03 -0700

    Initial Commit

---
+++ code.ts
@@ -0,0 +1,45 @@
+
+const command = figma.command;
+
+function getInstance(item:BaseNode):InstanceNode|null {
+  console.log("getInstance", item);
+  if(item.parent.type === "INSTANCE") {
+    return item.parent;
+  }
+  else if((item.parent as SceneNode).type) {
+    return getInstance(item.parent)
+  }
+  else return null;
+}
+
+function getOuterInstance(item:BaseNode):InstanceNode|null {
+  let outerInstance:InstanceNode|null = null;
+  if(item.type === "INSTANCE") {
+    outerInstance = item;
+  }
+  let checkItem = item;
+  while(checkItem.parent) {
+    if(checkItem.parent.type === "INSTANCE") {
+      outerInstance = checkItem.parent;
+    }
+    checkItem = checkItem.parent;
+  }
+  return outerInstance;
+  
+}
+
+let instances = [];
+
+if(command==="containing") {
+  instances = figma.currentPage.selection.map(selectedItem=>getInstance(selectedItem)).filter(val=>val!==null);
+  
+}
+else if(command==="top") {
+  instances = figma.currentPage.selection.map(selectedItem=>getOuterInstance(selectedItem)).filter(val=>val!==null);
+}
+
+figma.currentPage.selection = instances;
+
+// Make sure to close the plugin when you're done. Otherwise the plugin will
+// keep running, which shows the cancel button at the bottom of the screen.
+figma.closePlugin();
